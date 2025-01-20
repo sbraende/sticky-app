93e3743c5441e9e1ed5ad8e014231e2320b3e7a2
sbraende <sebastian.braende@gmail.com>
2025-01-20 09:13:21 +0100

    Add inital render

--- src/js/addNotes.js
+++ src/js/addNotes.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 import storeNotes from "./storeNotes.js";
 
 // Math.random() * (Max - min +1) + min
@@ -8,7 +8,7 @@ export const generateRandomNumber = () =>
 const addNotes = (subject, date, text) => {
   const notesArray = JSON.parse(localStorage.getItem("notes")) || [];
   const note = {
-    id: uuidv4(),
+    // id: uuidv4(),
     subject: subject.value.trim(),
     date: date.value.trim(),
     text: text.value.trim(),
--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,9 @@ const subjectInput = document.querySelector(".form__subject-input");
 const dateInput = document.querySelector(".form__date-input");
 const noteText = document.querySelector(".form__note-input");
 
+// Inital render
+document.addEventListener("DOMContentLoaded", renderNotes);
+
 // Add submit event to form
 form.addEventListener("submit", (event) => {
   event.preventDefault();
